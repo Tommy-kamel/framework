5f47c219c29eabdec7f0b94c9d6c8724c938511c
Tommy-kamel <167763345+Tommy-kamel@users.noreply.github.com>
2025-09-23 22:21:05 +0300

    Jar FrontServlet : test deploye

---
+++ src/main/java/com/example/FrontServlet.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FrontServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Requete geree par FrontServlet pour l'URL : " + req.getRequestURI());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
