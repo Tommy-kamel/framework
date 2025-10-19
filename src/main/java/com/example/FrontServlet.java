e5c3a0179625c4a5864d7cc075d1a63d2788ddfc
Tommy-kamel <167763345+Tommy-kamel@users.noreply.github.com>
2025-10-19 22:08:37 +0300

    Modification frontServlet sprint 1 bis

--- src/main/java/com/example/FrontServlet.java
+++ src/main/java/com/example/FrontServlet.java
@@ -1,20 +1,43 @@
 package com.example;
 
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class FrontServlet extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String uri = req.getRequestURI();
-        resp.getWriter().println("URL : " + uri);
+    public void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        boolean ressourceContext = getServletContext().getResource(path)!=null;
+        if(!ressourceContext){
+            response.getWriter().println(path);
+        }
+        else{
+            if(path.endsWith(".jsp")){
+                RequestDispatcher dispatcher = getServletContext().getNamedDispatcher("jsp");
+                dispatcher.forward(request, response);
+            }
+            else{
+                response.getWriter().println(path);
+            }
+        }
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        service(request, response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        service(request, response);
     }
 }
